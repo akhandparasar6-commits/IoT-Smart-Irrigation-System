08f658c2a5c4e02eebb2ac33e7b9ef22bc15cb08
AKHAND PARASAR <akhandparasar6@gmail.com>
2026-05-19 17:44:33 +0530

    Enhance code readability with comments
    Added comments for clarity and improved code readability.

--- smart_irrigation.ino
+++ smart_irrigation.ino
@@ -1,22 +1,31 @@
-#define sensorPin A0
-#define relayPin D1
+// assigning names to the pins using define
+#define sensorPin A0;  
+#define relayPin D1;  
 
+// variable declaration
 int moistureValue;
-int threshold = 700;   // Adjust based on calibration
+int threshold = 700;   // Adjust based on calibration + Soil Type + sensor type
 
+// setup() fxn runs only once 
+// pin setup in this fxn
+// serial comunication
 void setup() {
-  pinMode(relayPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);  // because relay pin will send signals
   digitalWrite(relayPin, HIGH);   // Pump OFF initially
-  Serial.begin(9600);
+  Serial.begin(9600); // serial communication between PC and ESP32
 }
 
+// RUNS CONTINUOUSLY FOREVER
 void loop() {
 
-  moistureValue = analogRead(sensorPin);
+  // analogRead() -> analog to digital
+  moistureValue = analogRead(sensorPin);  // moisture value store sensor data in digital form 
   
+  // prints value on serial monitor
   Serial.print("Soil Moisture Value: ");
   Serial.println(moistureValue);
 
+  // decision making 
   if (moistureValue > threshold) {  
     Serial.println("Soil is Dry - Pump ON");
     digitalWrite(relayPin, LOW);    // Turn pump ON
@@ -26,5 +35,5 @@ void loop() {
     digitalWrite(relayPin, HIGH);   // Turn pump OFF
   }
 
-  delay(2000);
+  delay(2000); // 2 sec break before next reading
 }
